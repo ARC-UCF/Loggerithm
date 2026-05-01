f66f1c07174614789510723c567dbeb1fc545d2b
divinefoxx <divine.foxx0@gmail.com>
2026-05-01 11:13:44 -0400

    Updated navbar to now reflect callsign of user

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,8 +1,23 @@
 import { Link } from "react-router-dom";
 import { useModal } from "../modals/ModalProvider";
+import { useEffect, useState } from "react";
 
 export default function Navbar() {
     const { openModal } = useModal();
+
+    const [call, setCall] = useState("");
+
+    function fetchCall() {
+        let storedCall = localStorage.getItem("callsign") as string;
+
+        if (storedCall) {
+            setCall(storedCall);
+        }
+    }
+
+    useEffect(() => {
+        fetchCall();
+    }, []);
 
     return (
         <nav>
@@ -13,7 +28,8 @@ export default function Navbar() {
                 <li><a onClick={() => openModal("operator")}>Operator</a></li>
                 <li><a><Link to="/audit-logs">Audit Logs</Link></a></li>
             </ul>
-            <p>Hi, K9SRH!</p>
+            {call && <p>Hi, {call}!</p>}
+            {!call && <p>Please login!</p>}
         </nav>
     );
 }
