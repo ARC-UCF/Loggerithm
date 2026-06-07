be817b6656232f8cb21dcc1f822dfb8c73f47d69
divinefoxx <divine.foxx0@gmail.com>
2026-06-07 12:54:43 -0400

    Updated Logger to use Activity instead of useState to keep field input persistent while on the log page.

--- src/Pages/Logger.tsx
+++ src/Pages/Logger.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { Activity, useState } from "react";
 import Home from "./Home";
 import POTA from "./POTA";
 import FieldDay from "./FieldDay";
@@ -7,12 +7,23 @@ import NormalLog from "./NormalLog";
 export default function Logger() {
     const [view, setView] = useState<"home" | "pota" | "field" | "normal">("home");
 
+    // We use the Activity component from React now to keep the component states the same when we switch between them. This prevents the erasure of form inputs when navigating the log page.
+    // We still need to implement components to prevent the change of the user's location.
+
     return (
         <>
-            {view === "home" && <Home setView={setView}/>}
-            {view === "pota" && <POTA setView={setView}/>}
-            {view === "field" && <FieldDay setView={setView} />}
-            {view === "normal" && <NormalLog setView={setView} />}
+            <Activity mode={view === "home" ? "visible" : "hidden"}>
+                <Home setView={setView} />
+            </Activity>
+            <Activity mode={view === "pota" ? "visible" : "hidden"}>
+                <POTA setView={setView} />
+            </Activity>
+            <Activity mode={view === "field" ? "visible" : "hidden"}>
+                <FieldDay setView={setView} />
+            </Activity>
+            <Activity mode={view === "normal" ? "visible" : "hidden"}>
+                <NormalLog setView={setView}/>
+            </Activity>
         </>
     );
 }
